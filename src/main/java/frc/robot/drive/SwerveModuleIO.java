da196a3d948ed87eb5bc83c83f4b048d26c22190
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-03-28 02:48:30 -0400

    Add SwerveModuleIO and implements

---
+++ src/main/java/frc/robot/drive/SwerveModuleIO.java
@@ -0,0 +1,40 @@
+package frc.robot.drive;
+
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.lib.LoggedIO;
+import frc.robot.constants.Constants;
+import org.littletonrobotics.junction.AutoLog;
+
+public interface SwerveModuleIO extends LoggedIO<SwerveModuleIO.SwerveModuleInputs> {
+  @AutoLog
+  public class SwerveModuleInputs {
+    public double DriveCurrentAmps;
+    public double DriveTempCelsius;
+
+    public double TurnPositionRad;
+    public double TurnVelocityRadPerSec;
+    public double TurnCurrentAmps;
+    public double TurnTempCelsius;
+
+    public double AbsoluteModulePositionRad;
+    public double DistanceTraveledMeters;
+    public double VelocityMetersPerSecond;
+  }
+
+  default SwerveModuleState getState() {
+    return new SwerveModuleState();
+  }
+
+  default SwerveModulePosition getPosition() {
+    return new SwerveModulePosition();
+  }
+
+  default void setState(SwerveModuleState state) {}
+
+  default void resyncEncoders() {}
+
+  default void stop() {}
+
+  default void setNeutralMode(Constants.NeutralMode driveNeutralMode, Constants.NeutralMode steerNeutralMode) {}
+}
