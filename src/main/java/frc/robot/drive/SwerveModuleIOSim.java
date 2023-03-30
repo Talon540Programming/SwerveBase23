0fa45a238428348a8969c6224ae7e5d6b2af6b5d
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-03-30 11:15:57 -0400

    Simplify SwerveModuleSim

--- src/main/java/frc/robot/drive/SwerveModuleIOSim.java
+++ src/main/java/frc/robot/drive/SwerveModuleIOSim.java
@@ -1,91 +1,11 @@
 package frc.robot.drive;
-//
-// import edu.wpi.first.math.controller.PIDController;
-// import edu.wpi.first.math.geometry.Rotation2d;
-// import edu.wpi.first.math.kinematics.SwerveModulePosition;
-// import edu.wpi.first.math.kinematics.SwerveModuleState;
-// import edu.wpi.first.math.system.plant.DCMotor;
-// import edu.wpi.first.math.util.Units;
-// import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-// import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-// import frc.robot.constants.Constants;
-//
-// public class SwerveModuleIOSim implements SwerveModuleIO {
-//     // private final FlywheelSim m_driveGearbox = new FlywheelSim(DCMotor.getNEO(1),
-// Constants.Drivetrain.kDriveGearRatio, 0.025);
-//     // private final FlywheelSim m_steerGearbox = new FlywheelSim(DCMotor.getNEO(1),
-// Constants.Drivetrain.kSteerGearRatio, 0.004);
-//
-//     private final DCMotorSim m_driveGearbox = new DCMotorSim(DCMotor.getNEO(1),
-// Constants.Drivetrain.kDriveGearRatio, 0.025);
-//     private final DCMotorSim m_steerGearbox = new DCMotorSim(DCMotor.getNEO(1),
-// Constants.Drivetrain.kSteerGearRatio, 0.004);
-//
-//     private double turnRelativePositionRad = 0.0;
-//     private double turnAbsolutePositionRad = Math.random() * 2.0 * Math.PI;
-//     private double driveAppliedVolts = 0.0;
-//     private double turnAppliedVolts = 0.0;
-//
-//     private final PIDController m_driveController = new PIDController(
-//             Constants.Drivetrain.ControlValues.Drive.kP,
-//             Constants.Drivetrain.ControlValues.Drive.kI,
-//             Constants.Drivetrain.ControlValues.Drive.kD
-//     );
-//     private final PIDController m_steerController = new PIDController(
-//             Constants.Drivetrain.ControlValues.Steer.kP,
-//             Constants.Drivetrain.ControlValues.Steer.kI,
-//             Constants.Drivetrain.ControlValues.Steer.kD
-//     );
-//
-//     public SwerveModuleIOSim() {
-//         m_steerController.enableContinuousInput(-Math.PI, Math.PI);
-//     }
-//
-//     @Override
-//     public void updateInputs(SwerveModuleInputs inputs) {
-//         m_driveGearbox.update(Constants.loopPeriodSecs);
-//         m_steerGearbox.update(Constants.loopPeriodSecs);
-//
-//         inputs.DriveCurrentAmps = m_driveGearbox.getCurrentDrawAmps();
-//
-//         inputs.TurnPositionRad = m_steerGearbox.getAngularPositionRad();
-//         inputs.TurnVelocityRadPerSec = m_steerGearbox.getAngularVelocityRadPerSec();
-//
-//         // Units.radiansToRotations(0) * Constants.Drivetrain.kDriveMotorConversionFactor;
-//
-//
-//
-//     }
-//
-//     @Override
-//     public SwerveModulePosition getPosition() {
-//         return SwerveModuleIO.super.getPosition();
-//     }
-//
-//     @Override
-//     public SwerveModuleState getState() {
-//         return SwerveModuleIO.super.getState();
-//     }
-//
-//     @Override
-//     public void setState(SwerveModuleState state) {
-//         // Optimize the SwerveModuleState
-//         state = SwerveModuleState.optimize(state,
-// Rotation2d.fromRadians(turnAbsolutePositionRad));
-//
-//         double driveOutput = m_driveController.calculate();
-//     }
-// }
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.constants.Constants;
 
 public class SwerveModuleIOSim implements SwerveModuleIO {
   private final SwerveModuleState m_currentState = new SwerveModuleState();
-  private SwerveModuleState m_desiredState = new SwerveModuleState();
   private final SwerveModulePosition m_currentPosition = new SwerveModulePosition();
 
   @Override
@@ -102,7 +22,8 @@ public class SwerveModuleIOSim implements SwerveModuleIO {
   public void setState(SwerveModuleState state) {
     state = SwerveModuleState.optimize(state, m_currentState.angle);
 
-    m_desiredState = state;
+    m_currentState.speedMetersPerSecond = state.speedMetersPerSecond;
+    m_currentState.angle = state.angle;
   }
 
   @Override
@@ -112,25 +33,14 @@ public class SwerveModuleIOSim implements SwerveModuleIO {
 
   @Override
   public void updateInputs(SwerveModuleInputs inputs) {
-    double oldAngleRad = m_currentPosition.angle.getRadians();
-    double updatedAngle =
-        MathUtil.interpolate(
-            m_currentPosition.angle.getRadians(), m_desiredState.angle.getRadians(), 1);
-
-    // Update State
-    m_currentState.angle = Rotation2d.fromRadians(updatedAngle);
-    m_currentState.speedMetersPerSecond = m_desiredState.speedMetersPerSecond;
-
-    // Update Pose
+    // Update Position
     m_currentPosition.distanceMeters +=
         m_currentState.speedMetersPerSecond * Constants.loopPeriodSecs;
     m_currentPosition.angle = m_currentState.angle;
 
     inputs.DistanceTraveledMeters = m_currentPosition.distanceMeters;
     inputs.VelocityMetersPerSec = m_currentState.speedMetersPerSecond;
-    inputs.VelocityRadPerSec =
-        (m_currentPosition.angle.getRotations() - oldAngleRad) / Constants.loopPeriodSecs;
-    inputs.RelativePositionRad = m_currentPosition.angle.getRadians();
+
     inputs.AbsolutePositionRad = m_currentPosition.angle.getRadians();
   }
 }
