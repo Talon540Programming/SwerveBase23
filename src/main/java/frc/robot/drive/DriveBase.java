905141050fbeb7a6350cf003692ea2706de2e649
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-04-16 12:18:01 -0400

    Update DriveBase.java

--- src/main/java/frc/robot/drive/DriveBase.java
+++ src/main/java/frc/robot/drive/DriveBase.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.PoseEstimator;
 import frc.robot.constants.Constants;
 import frc.robot.drive.gyro.GyroIO;
+import frc.robot.drive.gyro.GyroInputsAutoLogged;
 import org.littletonrobotics.junction.Logger;
 
 public class DriveBase extends SubsystemBase {
